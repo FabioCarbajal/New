e79f87dd57f49202beed5d0d72b046b7fccbe2e8
FabioCarbajal <fabio.carbajal@ucsp.edu.pe>
2017-06-08 16:03:06 -0500

    Add files via upload

--- PointArray/include/PointArray.h
+++ PointArray/include/PointArray.h
@@ -5,21 +5,22 @@
 class PointArray
 {
     private:
-        Point *puntero;
+        Point *puntos;
         int lenght;
+        void relenght(int lenght);
     public:
         PointArray();
-        PointArray(Point points[], const int lenght);
+        PointArray(Point pts[], const int lenght);
         PointArray(const PointArray &pv);
         ~PointArray();
-        void relenght(int n);
-        void push_back(const int &puntero);
-        void insert(const int position, const Point &puntero);
-        void remove(const int pos);
-        const int getlenght();
+
         void clear();
-        Point get(const int position);
-        const Point get(const int position) const;
+        int getlenght() const {return lenght;}
+        void push_back(const Point &p);
+        void insert(const int position, const Point &p);
+        void remove(const int position);
+        Point *get(const int position);
+        const Point *get(const int position) const;
 };
 
 #endif // POINTARRAY_H
--- PointArray/src/PointArray.cpp
+++ PointArray/src/PointArray.cpp
@@ -3,46 +3,83 @@
 PointArray::PointArray()
 {
     this->lenght=0;
-    puntero=new Point[lenght];
+    puntos=new Point[lenght];
 }
 
-PointArray::PointArray(Point points[], const int lenght)
+PointArray::PointArray(Point ptscopia[], const int copialenght)
 {
-    puntero=points;
     this->lenght=lenght;
+    puntos=new Point[copialenght];
+    for(int i=0;i<lenght;++i)
+    {
+        puntos[i]=ptscopia[i];
+    }
 }
 
 PointArray::PointArray(const PointArray &pv)
 {
     this->lenght=pv.lenght;
-    this->puntero=pv.puntero;
+    this->puntos=pv.puntos;
 }
 
 PointArray::~PointArray()
 {
-    for(int i=0;i<lenght;i++)
-    {
-        (puntero++)->setX(0);
-        (puntero++)->setY(0);
-    }
-    delete[] puntero;
+    delete[] puntos;
 }
 
-void PointArray::relenght(int n)
+void PointArray::relenght(int nuevolenght)
 {
-    delete[] puntero;
-    Point arreglo2[n];
-    if(n<lenght)
-        for(int i=0; i<n; i++){
-            arreglo2[i]=(puntero++)->getX();
-            arreglo2[i]=(puntero++)->getY();
-        }
-    puntero=arreglo2;
-    this->lenght=n;
+    Point *pts= new Point[nuevolenght];
+    int minlenght=(nuevolenght>lenght?lenght:nuevolenght);
+    for(int i=0;i<minlenght;i++)
+        pts[i]=puntos[i];
+    delete[] puntos;
+    lenght=nuevolenght;
+    puntos=pts;
 }
 
+void PointArray::clear()
+{
+    relenght(0);
+}
 
+void PointArray::push_back(const Point &p)
+{
+    relenght(lenght+1);
+    puntos[lenght-1]=p;
+}
 
+void PointArray::insert(const int position, const Point &p)
+{
+    relenght(lenght+1);
+    for(int i=lenght-1;i>position;i--)
+    {
+        puntos[i]=puntos[i-1];
+    }
+    puntos[position]=p;
+}
+
+void PointArray::remove(const int position)
+{
+    if(position >= 0 && position<lenght)
+    {
+        for(int i=position;i<lenght-2;i++)
+        {
+            puntos[i]=puntos[i+1];
+        }
+        relenght(lenght-1);
+    }
+}
+
+Point *PointArray::get(const int position)
+{
+    return position >=0 && position<lenght ? puntos + position:NULL;
+}
+
+const Point *PointArray::get(const int position) const
+{
+    return position >= 0 && position<lenght?puntos+position:;
+}
 
 
 
